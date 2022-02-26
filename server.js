04900b28e60edaa54a8b479a66670a3bfb9408a2
Chubbs91 <chaskin1991@outlook.com>
2022-02-26 14:13:24 -0600

    server set-up

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const sequelize = require('./config/connection');
 const path = require('path');
+const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Setup Session Here
+// PLACEHOLDER COMMENT
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// Turn on Connection to DB and Server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));
+});
