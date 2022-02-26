e6dd89ca25406aa862df925a7c4682763a28d586
Chubbs91 <chaskin1991@outlook.com>
2022-02-25 23:46:02 -0600

    implement CRUD actions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,15 +13,74 @@ router.get('/', (req, res) => {
 });
 
 // GET User by ID
-router.get('/:id', (req, res) => {});
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        },
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No User found with this ID'});
+            return;
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // POST/CREATE a New User
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // PUT/UPDATE a User by ID
-router.put('/:id', (req, res) => {});
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No User found with this ID' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+});
 
 // DELETE a User by ID
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No User found with this ID' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
