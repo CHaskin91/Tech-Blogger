185dd284a68c942eb46cddb8e61b4b7a5394ecb8
Chubbs91 <chaskin1991@outlook.com>
2022-02-27 14:34:42 -0600

    DELETE a post added

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -87,4 +87,24 @@ router.put('/:id', (req, res) => {
         });
 });
 
+// DELETE a Post
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this ID' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
