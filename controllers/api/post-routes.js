e384917b451dcaf1d611d954cf8ca9034e0013b1
Chubbs91 <chaskin1991@outlook.com>
2022-02-27 14:23:52 -0600

    GET a single post created

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -20,4 +20,31 @@ router.get('/', (req, res) => {
     });
 });
 
+// Get a Single Post
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'post_text', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this ID' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
