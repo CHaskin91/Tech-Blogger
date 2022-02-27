ccc2029b163f583b20d151b74e122dd0e14e4267
Chubbs91 <chaskin1991@outlook.com>
2022-02-27 16:13:45 -0600

    installed handlebars.  creating home-routes currently

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const path = require('path');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,8 +10,14 @@ const PORT = process.env.PORT || 3001;
 // Setup Session Here
 // PLACEHOLDER COMMENT
 
+// Handlebars Initialization
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
