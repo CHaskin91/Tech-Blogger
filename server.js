7f0e89b590321d8a04cc297588173b221b07007e
Chubbs91 <chaskin1991@outlook.com>
2022-02-28 14:17:40 -0600

    helpers created. partials finished

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 require('dotenv').config();
 
@@ -30,6 +31,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
