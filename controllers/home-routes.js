474c496f3c99e4e2659f965d6529658645cdc99c
Chubbs91 <chaskin1991@outlook.com>
2022-02-28 13:55:33 -0600

    write conditionals using handlebars.js syntax

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -47,4 +47,56 @@ router.get('/', (req, res) => {
     res.render('login');
   });
 
+  router.get('/post/:id', (req, res) => {
+      Post.findOne({
+          where: {
+              id: req.params.id
+          },
+          attributes: [
+              'id',
+              'title',
+              'post_text',
+              'created_at'
+          ],
+          include: [
+              {
+                  model: Comment,
+                  attributes: [
+                      'id',
+                      'comment_text',
+                      'post_id',
+                      'user_id',
+                      'created_at'
+                  ],
+                  include: {
+                      model: User,
+                      attributes: ['username']
+                  }
+              },
+              {
+                  model: User,
+                  attributes: ['username']
+              }
+          ]
+      })
+      .then(dbPostData => {
+          if (!dbPostData) {
+              res.status(404).json({ message: 'No Post found with this ID' });
+              return;
+          }
+          // Serialize the Data
+          const post = dbPostData.get({ plain: true });
+
+          // Pass Data to the Template
+          res.render('single-post', {
+              post,
+              loggedIn: req.session.loggedIn
+          });
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      }); 
+  });
+
 module.exports = router;
---
+++ public/javascript/add-post.js
@@ -0,0 +1,6 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
